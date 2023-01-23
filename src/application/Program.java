404ae79727cbd80864c3b543a7c3ecb3be41a33c
gabDomingues <gabrieldomingues123@hotmail.com.br>
2023-01-23 14:47:58 -0300

    Solucao ruim usando String como retorno do erro

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,24 +26,19 @@ public class Program {
 			Reserva reserva = new Reserva(numero, checkIn, checkOut);
 			System.out.println(reserva);
 			System.out.println();
-			System.out.print("Atualizar reserva");
+			System.out.println("Atualizar reserva");
 			System.out.println("Check-In (dd/mm/aaaa): ");
 			checkIn = LocalDate.parse(sc.next(), fmt);
 			System.out.println("Check-Out (dd/mm/aaaa): ");
 			checkOut = LocalDate.parse(sc.next(), fmt);
 
-			LocalDate agora = LocalDate.now();
-			if (checkIn.isBefore(agora) || checkOut.isBefore(agora)) {
-				System.out.println("Erro na reserva. Datas atualizadas devem ser futuras.");
-			} else if (!checkOut.isAfter(checkIn)) {
-				System.out.println("Erro na reserva. Data de Check-Out deve ser posterior Check-In");
-			} else {
-				reserva.atualizarDatas(checkIn, checkOut);
+			String erro = reserva.atualizarDatas(checkIn, checkOut);
+			if( erro != null) {
+				System.out.println(erro);
+			}else {
 				System.out.println(reserva);
 			}
-
 		}
-
 		sc.close();
 	}
 
--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -5,15 +5,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Reserva {
-	
+
 	private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
+
 	private Integer numeroQuarto;
 	private LocalDate checkIn;
 	private LocalDate checkOut;
-	
+
 	public Reserva() {
-		
+
 	}
 
 	public Reserva(Integer numeroQuarto, LocalDate checkIn, LocalDate checkOut) {
@@ -38,20 +38,28 @@ public class Reserva {
 		return checkOut;
 	}
 
-	
 	public long duracao() {
 		Duration duracao = Duration.between(this.getcheckIn().atStartOfDay(), this.getcheckOut().atStartOfDay());
 		return duracao.toDays();
 	}
-	
-	public void atualizarDatas(LocalDate checkIn, LocalDate checkOut) {
+
+	public String atualizarDatas(LocalDate checkIn, LocalDate checkOut) {
+
+		LocalDate agora = LocalDate.now();
+		if (checkIn.isBefore(agora) || checkOut.isBefore(agora)) {
+			return "Erro na reserva. Datas atualizadas devem ser futuras.";
+		}
+		if (!checkOut.isAfter(checkIn)) {
+			return "Erro na reserva. Data de Check-Out deve ser posterior Check-In";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
-	
+
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Reserva: Quarto " );
+		sb.append("Reserva: Quarto ");
 		sb.append(this.getNumeroQuarto() + ", ");
 		sb.append("Check-In: " + this.getcheckIn().format(fmt) + ", ");
 		sb.append("Check-Out: " + this.getcheckOut().format(fmt) + ", ");
