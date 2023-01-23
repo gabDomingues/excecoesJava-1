62e8ba1a319396e6fc30a4df308306db94b506fd
gabDomingues <gabrieldomingues123@hotmail.com.br>
2023-01-23 14:36:57 -0300

    Solucao muito ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,21 +2,49 @@ package application;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 import model.entities.Reserva;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
-		Reserva r1 = new Reserva ();
-		String data = "23/01/2023";
-		String data2 = "26/01/2023";
-		
-		r1 = new Reserva(101, LocalDate.parse(data, fmt), LocalDate.parse(data2, fmt) );
-		System.out.println(r1);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Numero quarto: ");
+		int numero = sc.nextInt();
+		System.out.println("Check-In (dd/mm/aaaa): ");
+		LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
+		System.out.println("Check-Out (dd/mm/aaaa): ");
+		LocalDate checkOut = LocalDate.parse(sc.next(), fmt);
+
+		if (!checkOut.isAfter(checkIn)) {
+			System.out.println("Erro na reserva. Data de Check-Out deve ser posterior Check-In");
+		} else {
+			Reserva reserva = new Reserva(numero, checkIn, checkOut);
+			System.out.println(reserva);
+			System.out.println();
+			System.out.print("Atualizar reserva");
+			System.out.println("Check-In (dd/mm/aaaa): ");
+			checkIn = LocalDate.parse(sc.next(), fmt);
+			System.out.println("Check-Out (dd/mm/aaaa): ");
+			checkOut = LocalDate.parse(sc.next(), fmt);
+
+			LocalDate agora = LocalDate.now();
+			if (checkIn.isBefore(agora) || checkOut.isBefore(agora)) {
+				System.out.println("Erro na reserva. Datas atualizadas devem ser futuras.");
+			} else if (!checkOut.isAfter(checkIn)) {
+				System.out.println("Erro na reserva. Data de Check-Out deve ser posterior Check-In");
+			} else {
+				reserva.atualizarDatas(checkIn, checkOut);
+				System.out.println(reserva);
+			}
+
+		}
+
+		sc.close();
 	}
 
 }
